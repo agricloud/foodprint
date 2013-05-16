84011999613c72b90f887fbd772c97c8fd471d39
demo <argicloud.cc@gmail.com>
2013-05-16 17:20:00 +0800

    test

--- grails-app/domain/finder/Item.groovy
+++ grails-app/domain/finder/Item.groovy
@@ -3,23 +3,23 @@ package finder
 class Item {
 
 
-		String name
-		String title=""
-		String description=""
-		Long dueDays
-		Date effectStartDate
-		Date effectEndDate
+	String name
+	String title=""
+	String description=""
+	Long dueDays
+	Date effectStartDate
+	Date effectEndDate
 
-		String editor=""			//修改者
-		String creator=""		//建立者
-    Date dateCreated    //建立日期
-    Date lastUpdated    //修改日期
+	String editor=""			//修改者
+	String creator=""		//建立者
+	Date dateCreated    //建立日期
+	Date lastUpdated    //修改日期
 
-    static hasMany=[itemImages:ItemImage]
-	
-    static constraints = {
-    	dueDays nullable:true
-    	effectStartDate nullable:true
-    	effectEndDate nullable:true
-    }
+	static hasMany=[itemImages:ItemImage]
+	  
+	static constraints = {
+		dueDays nullable:true
+		effectStartDate nullable:true
+		effectEndDate nullable:true
+	}
 }
