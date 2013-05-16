3607812e95593d13694ef90dcf34e3ccd63fa97d
demo <argicloud.cc@gmail.com>
2013-05-16 17:17:40 +0800

    test

--- grails-app/domain/finder/Batch.groovy
+++ grails-app/domain/finder/Batch.groovy
@@ -2,19 +2,19 @@ package finder
 
 class Batch {
 
+	static belongsTo =[item:Item]
 
-		static belongsTo =[item:Item]
+	Long expectQty=0
+	String name
 
-		Long expectQty=0
-		String name
+	Date dueDate
 
-		Date dueDate
-
-		String editor=""			//修改者
-		String creator=""		//建立者
-    Date dateCreated    //建立日期
-    Date lastUpdated    //修改日期
-    static constraints = {
-    	dueDate nullable:true
-    }
+	String editor=""			//修改者
+	String creator=""		//建立者
+	Date dateCreated    //建立日期
+	Date lastUpdated    //修改日期
+	static constraints = {
+		dueDate nullable:true
+	}
+	
 }
