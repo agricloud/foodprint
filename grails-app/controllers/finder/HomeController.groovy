92d273e2db700f8d83236f7da0b7f9f27cfcdbf8
Spooky <smlsun@gmail.com>
2013-05-16 22:27:52 +0800

    extjs update

---
+++ grails-app/controllers/finder/HomeController.groovy
@@ -0,0 +1,14 @@
+package finder
+
+class HomeController {
+
+    def index() { 
+      if (Environment.current == Environment.DEVELOPMENT) {
+         redirect(uri: "/development/app.html")
+      }
+      else {
+         redirect(uri: "/production/index.html")
+      }
+
+    }
+}
