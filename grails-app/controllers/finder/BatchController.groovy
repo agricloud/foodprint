dc600e04d6c195c2ac2c32378d8089c9f044162a
pipi <youbelu@gmail.com>
2013-07-29 19:02:32 +0800

    pipitest

--- grails-app/controllers/finder/BatchController.groovy
+++ grails-app/controllers/finder/BatchController.groovy
@@ -3,18 +3,34 @@ import grails.converters.JSON
 
 class BatchController {
 
-	def listAll = {
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        [batchInstanceList: Batch.list(params), batchInstanceTotal: Batch.count()]
+    }
+
+    def listJson(Integer max) {
+
+        render (contentType: 'text/json') {
+            list(max)        
+        }
+        
+    }
+
+    def listAll = {
 		
-		def batch=Batch.list()
+        def batch=Batch.list()
 
-
-
-		render (contentType: 'text/json') {
-      [
-				batchs: batch,
-				total: batch.size()
-			]
-		}
-		
-	}
+        render (contentType: 'text/json') {
+        [
+	batchs: batch,
+	total: batch.size()
+        ]
+        }	
+    }
 }
