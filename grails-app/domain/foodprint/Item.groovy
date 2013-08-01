c7f8581f528640499d70c1f4d775961e042541af
huang <chonghao1230@gmail.com>
2013-08-01 17:30:50 +0800

    #1 ItemBtnFn > selectItem > readItem > doRead > doCreate > doSave > doUpdate > doDelete

--- grails-app/domain/foodprint/Item.groovy
+++ grails-app/domain/foodprint/Item.groovy
@@ -2,39 +2,23 @@ package foodprint
 
 class Item {
 
-	///*
-	String name
-	String title=""
-	String description=""
-	Long effectiveDays
-	Date effectiveDate
-	Date ecxpiryDate
-	//*/
-	/*
 	String name
 	String title=""
 	String description=""
 	Long dueDays
 	Date effectStartDate
 	Date effectEndDate
-	*/
+
 	String editor=""			//修改者
 	String creator=""		//建立者
 	Date dateCreated    //建立日期
 	Date lastUpdated    //修改日期
 
 	static hasMany=[itemImages:ItemImage]
-	  
+
 	static constraints = {
-		///*
-		effectiveDays nullable:true
-		effectiveDate nullable:true
-		ecxpiryDate nullable:true
-		//*/
-		/*
 		dueDays nullable:true
 		effectStartDate nullable:true
 		effectEndDate nullable:true
-		*/
 	}
 }
