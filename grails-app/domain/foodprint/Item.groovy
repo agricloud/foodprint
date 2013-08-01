2c08c11e9be8b638628a2b930c89216b9c42c9ef
huang <chonghao1230@gmail.com>
2013-08-01 15:26:21 +0800

    btn

--- grails-app/domain/foodprint/Item.groovy
+++ grails-app/domain/foodprint/Item.groovy
@@ -2,14 +2,22 @@ package foodprint
 
 class Item {
 
-
+	///*
+	String name
+	String title=""
+	String description=""
+	Long effectiveDays
+	Date effectiveDate
+	Date ecxpiryDate
+	//*/
+	/*
 	String name
 	String title=""
 	String description=""
 	Long dueDays
 	Date effectStartDate
 	Date effectEndDate
-
+	*/
 	String editor=""			//修改者
 	String creator=""		//建立者
 	Date dateCreated    //建立日期
@@ -18,8 +26,15 @@ class Item {
 	static hasMany=[itemImages:ItemImage]
 	  
 	static constraints = {
+		///*
+		effectiveDays nullable:true
+		effectiveDate nullable:true
+		ecxpiryDate nullable:true
+		//*/
+		/*
 		dueDays nullable:true
 		effectStartDate nullable:true
 		effectEndDate nullable:true
+		*/
 	}
 }
