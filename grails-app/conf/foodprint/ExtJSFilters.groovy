25821c5bd9fa8dc8d5dfb21a526a324bccc65515
Spooky <smlsun@gmail.com>
2013-08-01 18:26:16 +0800

    Convert date into GORM properties recognization

--- grails-app/conf/foodprint/ExtJSFilters.groovy
+++ grails-app/conf/foodprint/ExtJSFilters.groovy
@@ -12,7 +12,22 @@ class ExtJSFilters {
 
                     // Transform value from Ext JS to Grails date style
                     if (value ==~ /^\d\d\d\d\-\d\d\-\d\dT\d\d:\d\d:\d\d$/) {
-                        params[key] = "${value}Z"
+
+                        // Convert into Date object
+                        //params[key] = Date.parse('yyyy-MM-dd HH:mm:ss', value.replaceFirst('T', ' '))
+
+                        // Convert into Grails style date string
+                        //params[key] = "${value}Z"
+                        
+                        // Convert into GORM properties recognization
+                        params[key] = 'date.struct'
+                        params["${key}_year"] = value.substring(0,4)
+                        params["${key}_month"] = value.substring(5,7)
+                        params["${key}_day"] = value.substring(8,10)
+
+                        params["${key}_hour"] = value.substring(11,13)
+                        params["${key}_minute"] = value.substring(14,16)
+                        params["${key}_second"] = value.substring(17,19)
 
                         log.info "Found ${value} is a Ext JS date format, transform into Grails style"
                         log.info "params[${key}] = ${value}"
