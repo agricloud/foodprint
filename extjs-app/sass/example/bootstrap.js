0327a6efa35503a9464401c0167ca01525febfff
Spooky <smlsun@gmail.com>
2013-08-06 18:30:27 +0800

    upgrate sencha cmd to v3.1.2.342

--- extjs-app/sass/example/bootstrap.js
+++ extjs-app/sass/example/bootstrap.js
@@ -4,3 +4,500 @@
  * provided to support globbing requires, custom xtypes, and other 
  * metadata-driven class system features 
  */
+Ext.Loader.addClassPathMappings({
+  "ExtThemeNeptune": "../../packages/ext-theme-neptune/overrides",
+  "Ext": "../../ext/src",
+  "Ext.rtl.EventObjectImpl": "../../ext/src/rtl/EventObject.js",
+  "foodprint": "../../app"
+});
+Ext.ClassManager.addNameAlternateMappings({
+  "foodprint.store.WorkstationStore": [],
+  "ExtThemeNeptune.container.ButtonGroup": [],
+  "foodprint.view.ItemEditor": [],
+  "foodprint.model.IndTcrRptRowImg": [],
+  "Ext.ux.ajax.DataSimlet": [],
+  "Ext.ux.grid.TransformGrid": [],
+  "foodprint.store.batchTrcRptStore": [],
+  "foodprint.store.TrcRptRowImgStore": [],
+  "foodprint.model.IndTrcRpt": [],
+  "foodprint.store.BatchChangeHistStore": [],
+  "foodprint.view.WorkstationGrid": [],
+  "ExtThemeNeptune.toolbar.Paging": [],
+  "foodprint.model.TrcRptRowImg": [],
+  "Ext.ux.event.Driver": [],
+  "Ext.ux.ajax.XmlSimlet": [],
+  "foodprint.view.DeleteBtn": [],
+  "foodprint.controller.BatchViewerController": [],
+  "Ext.ux.DataView.DragSelector": [],
+  "Ext.ux.grid.menu.ListMenu": [],
+  "foodprint.view.CreateBtn": [],
+  "Ext.ux.form.MultiSelect": [
+    "Ext.ux.Multiselect"
+  ],
+  "foodprint.store.BatchStore": [],
+  "foodprint.view.BatchViewer": [],
+  "foodprint.model.ItemTrcRpt": [],
+  "foodprint.model.BatchParams": [],
+  "foodprint.store.BatchTrcRptColStore": [],
+  "Ext.ux.PreviewPlugin": [],
+  "foodprint.view.BatchGrid": [],
+  "foodprint.model.IndTrcRptCol": [],
+  "Ext.ux.ajax.SimXhr": [],
+  "Ext.ux.event.RecorderManager": [],
+  "Ext.ux.ProgressBarPager": [],
+  "Ext.ux.CellDragDrop": [],
+  "Ext.ux.ajax.Simlet": [],
+  "Ext.ux.SlidingPager": [],
+  "Ext.ux.TabReorderer": [],
+  "Ext.ux.grid.filter.NumericFilter": [],
+  "foodprint.store.ItemStore": [],
+  "foodprint.store.IndTrcRptStore": [],
+  "ExtThemeNeptune.panel.Table": [],
+  "foodprint.store.IndTrcRptRowStore": [],
+  "Ext.ux.event.Recorder": [],
+  "foodprint.model.Report": [],
+  "foodprint.view.ParamEditor": [],
+  "foodprint.view.BatchTrcRptDisplay": [],
+  "foodprint.controller.BatchGridController": [],
+  "foodprint.view.ItemViewer": [],
+  "foodprint.view.Viewport": [],
+  "foodprint.store.IndTrcRptColStore": [],
+  "Ext.ux.ajax.SimManager": [],
+  "foodprint.store.TrcRptStore": [],
+  "foodprint.view.PickToolbar": [],
+  "foodprint.store.ItemImageStore": [],
+  "Ext.ux.grid.filter.BooleanFilter": [],
+  "foodprint.view.ItemEditorCt": [],
+  "foodprint.controller.ItemEditorController": [],
+  "foodprint.view.MainViewport": [],
+  "foodprint.model.TrcRptBodyDet": [],
+  "Ext.ux.TabScrollerMenu": [],
+  "ExtThemeNeptune.menu.Separator": [],
+  "foodprint.view.UploadBtn": [],
+  "Ext.ux.IFrame": [],
+  "foodprint.view.WorkstationEditor": [],
+  "foodprint.store.ItemParamsStore": [],
+  "foodprint.store.IndTrcRptRowImgStore": [],
+  "foodprint.view.IndTrcRptEditor": [],
+  "Ext.ux.form.SearchField": [],
+  "Ext.ux.dd.CellFieldDropZone": [],
+  "foodprint.store.BatchParamsStore": [],
+  "Ext.ux.LiveSearchGridPanel": [],
+  "foodprint.store.ItemTrcRptStore": [],
+  "Ext.ux.grid.filter.DateFilter": [],
+  "foodprint.model.IndTrcRptRow": [],
+  "foodprint.view.ReadBtn": [],
+  "foodprint.view.StdEditorToolbar": [],
+  "Ext.ux.TreePicker": [],
+  "foodprint.view.UpdateBtn": [],
+  "foodprint.model.ItemImage": [],
+  "ExtThemeNeptune.grid.RowEditor": [],
+  "ExtThemeNeptune.picker.Month": [],
+  "Ext.ux.data.PagingMemoryProxy": [
+    "Ext.data.PagingMemoryProxy"
+  ],
+  "foodprint.view.ItemExtendPanel": [],
+  "ExtThemeNeptune.menu.Menu": [],
+  "Ext.ux.BoxReorderer": [],
+  "foodprint.store.BatchTrcRptRowStore": [],
+  "foodprint.model.BatchTrcRptRow": [],
+  "Ext.ux.grid.filter.Filter": [],
+  "foodprint.controller.WorkstationGridController": [],
+  "foodprint.view.TrcRptGrid": [],
+  "Ext.ux.GroupTabRenderer": [],
+  "foodprint.model.batchChangeHist": [],
+  "foodprint.view.BatchEditorCt": [],
+  "foodprint.controller.WorkstationEditorController": [],
+  "foodprint.view.ReportGrid": [],
+  "Ext.ux.FieldReplicator": [],
+  "foodprint.view.BatchExtendPanel": [],
+  "ExtThemeNeptune.panel.Panel": [],
+  "Ext.ux.grid.filter.ListFilter": [],
+  "foodprint.controller.ItemGridController": [],
+  "foodprint.view.ParamEditorCt": [],
+  "foodprint.model.BatchTrcRptRowImg": [],
+  "foodprint.model.batchTrcRpt": [],
+  "foodprint.store.TrcRptBodyStore": [],
+  "foodprint.view.ItemGrid": [],
+  "Ext.ux.dd.PanelFieldDragZone": [],
+  "foodprint.model.Item": [],
+  "ExtThemeNeptune.toolbar.Toolbar": [],
+  "foodprint.model.Param": [],
+  "ExtThemeNeptune.tab.Tab": [],
+  "foodprint.store.TrcRptImgStroe": [],
+  "Ext.ux.GMapPanel": [],
+  "foodprint.store.TrcRptBodyDet": [],
+  "Ext.ux.GroupTabPanel": [],
+  "Ext.ux.DataView.LabelEditor": [],
+  "foodprint.view.ParamViewer": [],
+  "foodprint.model.ItemParams": [],
+  "foodprint.model.batch": [],
+  "foodprint.view.MainContainer": [],
+  "Ext.ux.grid.filter.StringFilter": [],
+  "Ext.ux.statusbar.ValidationStatus": [],
+  "Ext.ux.layout.Center": [],
+  "foodprint.store.TrcRptBody": [],
+  "foodprint.store.BatchTrcRptRowImgStore": [],
+  "Ext.ux.DataView.Animated": [],
+  "foodprint.controller.LoginController": [],
+  "foodprint.view.ParamGrid": [],
+  "ExtThemeNeptune.resizer.Splitter": [],
+  "foodprint.view.BatchEditor": [],
+  "foodprint.model.TrcRptBody": [],
+  "foodprint.model.TrcRpt": [],
+  "foodprint.view.ReportViewer": [],
+  "Ext.ux.statusbar.StatusBar": [
+    "Ext.ux.StatusBar"
+  ],
+  "foodprint.controller.ItemViewerController": [],
+  "foodprint.store.TrcRptBodyDetStore": [],
+  "foodprint.controller.WorkstationViewerController": [],
+  "foodprint.store.ParamStore": [],
+  "foodprint.view.ImageEditorToolbar": [],
+  "foodprint.view.TrcRptEditorCt": [],
+  "ExtThemeNeptune.layout.component.Dock": [],
+  "foodprint.model.TrcRptImg": [],
+  "foodprint.store.ReportStore": [],
+  "Ext.ux.grid.menu.RangeMenu": [],
+  "Ext.ux.RowExpander": [],
+  "foodprint.view.TrcRptViewer": [],
+  "Ext.ux.event.Player": [],
+  "foodprint.view.SaveBtn": [],
+  "ExtThemeNeptune.panel.Tool": [],
+  "Ext.ux.ToolbarDroppable": [],
+  "foodprint.view.LoginContainer": [],
+  "Ext.ux.event.Maker": [],
+  "foodprint.view.WrokstationViewer": [],
+  "foodprint.controller.BatchEditorController": [],
+  "Ext.ux.DataView.Draggable": [],
+  "foodprint.view.TrcRptEditor": [],
+  "Ext.ux.TabCloseMenu": [],
+  "foodprint.view.ReportEditorCt": [],
+  "foodprint.view.WorkstationEditorCt": [],
+  "foodprint.model.BatchTrcRptCol": [],
+  "Ext.ux.DataTip": [],
+  "Ext.ux.grid.filter.DateTimeFilter": [],
+  "Ext.ux.form.ItemSelector": [
+    "Ext.ux.ItemSelector"
+  ],
+  "foodprint.model.Wroksation": [],
+  "Ext.ux.ajax.JsonSimlet": [],
+  "foodprint.view.ReportEditor": [],
+  "Ext.ux.Spotlight": [],
+  "ExtThemeNeptune.form.field.HtmlEditor": [],
+  "Ext.ux.grid.FiltersFeature": [],
+  "ExtThemeNeptune.Component": []
+});
+Ext.ClassManager.addNameAliasMappings({
+  "foodprint.store.WorkstationStore": [],
+  "ExtThemeNeptune.container.ButtonGroup": [],
+  "foodprint.view.ItemEditor": [
+    "widget.itemeditor"
+  ],
+  "foodprint.model.IndTcrRptRowImg": [],
+  "Ext.ux.ajax.DataSimlet": [],
+  "Ext.ux.grid.TransformGrid": [],
+  "foodprint.store.batchTrcRptStore": [],
+  "foodprint.store.TrcRptRowImgStore": [],
+  "foodprint.model.IndTrcRpt": [],
+  "foodprint.store.BatchChangeHistStore": [],
+  "foodprint.view.WorkstationGrid": [
+    "widget.workstationgrid"
+  ],
+  "ExtThemeNeptune.toolbar.Paging": [],
+  "foodprint.model.TrcRptRowImg": [],
+  "Ext.ux.event.Driver": [],
+  "Ext.ux.ajax.XmlSimlet": [
+    "simlet.xml"
+  ],
+  "foodprint.view.DeleteBtn": [
+    "widget.deletebtn"
+  ],
+  "foodprint.controller.BatchViewerController": [],
+  "Ext.ux.DataView.DragSelector": [],
+  "Ext.ux.grid.menu.ListMenu": [],
+  "foodprint.view.CreateBtn": [
+    "widget.createbtn"
+  ],
+  "Ext.ux.form.MultiSelect": [
+    "widget.multiselect",
+    "widget.multiselectfield"
+  ],
+  "foodprint.store.BatchStore": [],
+  "foodprint.view.BatchViewer": [
+    "widget.batchviewer"
+  ],
+  "foodprint.model.ItemTrcRpt": [],
+  "foodprint.model.BatchParams": [],
+  "foodprint.store.BatchTrcRptColStore": [],
+  "Ext.ux.PreviewPlugin": [
+    "plugin.preview"
+  ],
+  "foodprint.view.BatchGrid": [
+    "widget.batchgrid"
+  ],
+  "foodprint.model.IndTrcRptCol": [],
+  "Ext.ux.ajax.SimXhr": [],
+  "Ext.ux.event.RecorderManager": [
+    "widget.eventrecordermanager"
+  ],
+  "Ext.ux.ProgressBarPager": [],
+  "Ext.ux.CellDragDrop": [
+    "plugin.celldragdrop"
+  ],
+  "Ext.ux.ajax.Simlet": [
+    "simlet.basic"
+  ],
+  "Ext.ux.SlidingPager": [],
+  "Ext.ux.TabReorderer": [],
+  "Ext.ux.grid.filter.NumericFilter": [
+    "gridfilter.numeric"
+  ],
+  "foodprint.store.ItemStore": [],
+  "foodprint.store.IndTrcRptStore": [],
+  "ExtThemeNeptune.panel.Table": [],
+  "foodprint.store.IndTrcRptRowStore": [],
+  "Ext.ux.event.Recorder": [],
+  "foodprint.model.Report": [],
+  "foodprint.view.ParamEditor": [
+    "widget.parameditor"
+  ],
+  "foodprint.view.BatchTrcRptDisplay": [],
+  "foodprint.controller.BatchGridController": [],
+  "foodprint.view.ItemViewer": [
+    "widget.itemviewer"
+  ],
+  "foodprint.view.Viewport": [],
+  "foodprint.store.IndTrcRptColStore": [],
+  "Ext.ux.ajax.SimManager": [],
+  "foodprint.store.TrcRptStore": [],
+  "foodprint.view.PickToolbar": [
+    "widget.picktoolbar"
+  ],
+  "foodprint.store.ItemImageStore": [],
+  "Ext.ux.grid.filter.BooleanFilter": [
+    "gridfilter.boolean"
+  ],
+  "foodprint.view.ItemEditorCt": [
+    "widget.itemeditorct"
+  ],
+  "foodprint.controller.ItemEditorController": [],
+  "foodprint.view.MainViewport": [],
+  "foodprint.model.TrcRptBodyDet": [],
+  "Ext.ux.TabScrollerMenu": [
+    "plugin.tabscrollermenu"
+  ],
+  "ExtThemeNeptune.menu.Separator": [],
+  "foodprint.view.UploadBtn": [
+    "widget.uploadbtn"
+  ],
+  "Ext.ux.IFrame": [
+    "widget.uxiframe"
+  ],
+  "foodprint.view.WorkstationEditor": [
+    "widget.workstationeditor"
+  ],
+  "foodprint.store.ItemParamsStore": [],
+  "foodprint.store.IndTrcRptRowImgStore": [],
+  "foodprint.view.IndTrcRptEditor": [],
+  "Ext.ux.form.SearchField": [
+    "widget.searchfield"
+  ],
+  "Ext.ux.dd.CellFieldDropZone": [],
+  "foodprint.store.BatchParamsStore": [],
+  "Ext.ux.LiveSearchGridPanel": [],
+  "foodprint.store.ItemTrcRptStore": [],
+  "Ext.ux.grid.filter.DateFilter": [
+    "gridfilter.date"
+  ],
+  "foodprint.model.IndTrcRptRow": [],
+  "foodprint.view.ReadBtn": [
+    "widget.readbtn"
+  ],
+  "foodprint.view.StdEditorToolbar": [
+    "widget.stdeditortoolbar"
+  ],
+  "Ext.ux.TreePicker": [
+    "widget.treepicker"
+  ],
+  "foodprint.view.UpdateBtn": [
+    "widget.updatebtn"
+  ],
+  "foodprint.model.ItemImage": [],
+  "ExtThemeNeptune.grid.RowEditor": [],
+  "ExtThemeNeptune.picker.Month": [],
+  "Ext.ux.data.PagingMemoryProxy": [
+    "proxy.pagingmemory"
+  ],
+  "foodprint.view.ItemExtendPanel": [
+    "widget.mytabpanel"
+  ],
+  "ExtThemeNeptune.menu.Menu": [],
+  "Ext.ux.BoxReorderer": [],
+  "foodprint.store.BatchTrcRptRowStore": [],
+  "foodprint.model.BatchTrcRptRow": [],
+  "Ext.ux.grid.filter.Filter": [],
+  "foodprint.controller.WorkstationGridController": [],
+  "foodprint.view.TrcRptGrid": [
+    "widget.trcrptgrid"
+  ],
+  "Ext.ux.GroupTabRenderer": [
+    "plugin.grouptabrenderer"
+  ],
+  "foodprint.model.batchChangeHist": [],
+  "foodprint.view.BatchEditorCt": [
+    "widget.batcheditorct"
+  ],
+  "foodprint.controller.WorkstationEditorController": [],
+  "foodprint.view.ReportGrid": [
+    "widget.reportgrid"
+  ],
+  "Ext.ux.FieldReplicator": [],
+  "foodprint.view.BatchExtendPanel": [
+    "widget.mytabpanel1"
+  ],
+  "ExtThemeNeptune.panel.Panel": [],
+  "Ext.ux.grid.filter.ListFilter": [
+    "gridfilter.list"
+  ],
+  "foodprint.controller.ItemGridController": [],
+  "foodprint.view.ParamEditorCt": [
+    "widget.parameditorct"
+  ],
+  "foodprint.model.BatchTrcRptRowImg": [],
+  "foodprint.model.batchTrcRpt": [],
+  "foodprint.store.TrcRptBodyStore": [],
+  "foodprint.view.ItemGrid": [
+    "widget.itemgrid"
+  ],
+  "Ext.ux.dd.PanelFieldDragZone": [],
+  "foodprint.model.Item": [],
+  "ExtThemeNeptune.toolbar.Toolbar": [],
+  "foodprint.model.Param": [],
+  "ExtThemeNeptune.tab.Tab": [],
+  "foodprint.store.TrcRptImgStroe": [],
+  "Ext.ux.GMapPanel": [
+    "widget.gmappanel"
+  ],
+  "foodprint.store.TrcRptBodyDet": [],
+  "Ext.ux.GroupTabPanel": [
+    "widget.grouptabpanel"
+  ],
+  "Ext.ux.DataView.LabelEditor": [],
+  "foodprint.view.ParamViewer": [
+    "widget.paramviewer"
+  ],
+  "foodprint.model.ItemParams": [],
+  "foodprint.model.batch": [],
+  "foodprint.view.MainContainer": [
+    "widget.maincontainer"
+  ],
+  "Ext.ux.grid.filter.StringFilter": [
+    "gridfilter.string"
+  ],
+  "Ext.ux.statusbar.ValidationStatus": [],
+  "Ext.ux.layout.Center": [
+    "layout.ux.center"
+  ],
+  "foodprint.store.TrcRptBody": [],
+  "foodprint.store.BatchTrcRptRowImgStore": [],
+  "Ext.ux.DataView.Animated": [],
+  "foodprint.controller.LoginController": [],
+  "foodprint.view.ParamGrid": [
+    "widget.paramgrid"
+  ],
+  "ExtThemeNeptune.resizer.Splitter": [],
+  "foodprint.view.BatchEditor": [
+    "widget.batcheditor"
+  ],
+  "foodprint.model.TrcRptBody": [],
+  "foodprint.model.TrcRpt": [],
+  "foodprint.view.ReportViewer": [
+    "widget.reportviewer"
+  ],
+  "Ext.ux.statusbar.StatusBar": [
+    "widget.statusbar"
+  ],
+  "foodprint.controller.ItemViewerController": [],
+  "foodprint.store.TrcRptBodyDetStore": [],
+  "foodprint.controller.WorkstationViewerController": [],
+  "foodprint.store.ParamStore": [],
+  "foodprint.view.ImageEditorToolbar": [
+    "widget.imageeditortoolbar"
+  ],
+  "foodprint.view.TrcRptEditorCt": [],
+  "ExtThemeNeptune.layout.component.Dock": [],
+  "foodprint.model.TrcRptImg": [],
+  "foodprint.store.ReportStore": [],
+  "Ext.ux.grid.menu.RangeMenu": [],
+  "Ext.ux.RowExpander": [],
+  "foodprint.view.TrcRptViewer": [
+    "widget.trcrptviewer"
+  ],
+  "Ext.ux.event.Player": [],
+  "foodprint.view.SaveBtn": [
+    "widget.savebtn"
+  ],
+  "ExtThemeNeptune.panel.Tool": [],
+  "Ext.ux.ToolbarDroppable": [],
+  "foodprint.view.LoginContainer": [
+    "widget.logincontainer1"
+  ],
+  "Ext.ux.event.Maker": [],
+  "foodprint.view.WrokstationViewer": [
+    "widget.workstationviewer"
+  ],
+  "foodprint.controller.BatchEditorController": [],
+  "Ext.ux.DataView.Draggable": [],
+  "foodprint.view.TrcRptEditor": [
+    "widget.trcrpteditor"
+  ],
+  "Ext.ux.TabCloseMenu": [
+    "plugin.tabclosemenu"
+  ],
+  "foodprint.view.ReportEditorCt": [
+    "widget.reporteditorct"
+  ],
+  "foodprint.view.WorkstationEditorCt": [
+    "widget.workstationeditorct"
+  ],
+  "foodprint.model.BatchTrcRptCol": [],
+  "Ext.ux.DataTip": [
+    "plugin.datatip"
+  ],
+  "Ext.ux.grid.filter.DateTimeFilter": [
+    "gridfilter.datetime"
+  ],
+  "Ext.ux.form.ItemSelector": [
+    "widget.itemselector",
+    "widget.itemselectorfield"
+  ],
+  "foodprint.model.Wroksation": [],
+  "Ext.ux.ajax.JsonSimlet": [
+    "simlet.json"
+  ],
+  "foodprint.view.ReportEditor": [
+    "widget.reporteditor"
+  ],
+  "Ext.ux.Spotlight": [],
+  "ExtThemeNeptune.form.field.HtmlEditor": [],
+  "Ext.ux.grid.FiltersFeature": [
+    "feature.filters"
+  ],
+  "ExtThemeNeptune.Component": []
+});
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/Component.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/panel/Panel.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/layout/component/Dock.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/toolbar/Toolbar.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/container/ButtonGroup.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/toolbar/Paging.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/picker/Month.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/form/field/HtmlEditor.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/panel/Table.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/grid/RowEditor.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/resizer/Splitter.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/menu/Menu.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/menu/Separator.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/panel/Tool.js");
+Ext.Loader.loadScript("../../packages/ext-theme-neptune/overrides/tab/Tab.js");
