182c334979425fb6874fce8fe8210681f5460709
Spooky <smlsun@gmail.com>
2013-09-17 18:11:18 +0800

    add domain BatchSource and add batch field

--- grails-app/domain/foodprint/Batch.groovy
+++ grails-app/domain/foodprint/Batch.groovy
@@ -3,11 +3,23 @@ package foodprint
 class Batch {
 
 	static belongsTo =[item:Item]
-	static hasMany =[batchRoutes:BatchRoute]
+	static hasMany =[batchRoutes:BatchRoute,batchSources:BatchSource]
 
 	String name
 	Long expectQty=0
 	Date dueDate
+
+
+    /*
+    * 製造完成日期
+    */
+	Date manufactureDate
+
+
+    /*
+    * 過期日期
+    */
+	Date expirationDate
 
 	Site site
 	String editor=""	//修改者
@@ -22,6 +34,8 @@ class Batch {
 		expectQty min:0L
 		
 		site nullable:true
+		manufactureDate nullable:true
+		expirationDate nullable:true
 	}
 	
 }
---
+++ grails-app/domain/foodprint/BatchSource.groovy
@@ -0,0 +1,13 @@
+package foodprint
+
+
+    /*
+    * 記錄生產的 batch 是由哪些元物料的 batch 所組成
+    */
+class BatchSource {
+
+	Batch batch
+
+    static constraints = {
+    }
+}
