96d265ba4c52a99bec69566d6f91d56a0b24f4a6
Pipi <pipi@Pipis-MacBook-Air.local>
2013-09-17 16:03:31 +0800

    image & form

--- grails-app/controllers/foodprint/ReportViewerController.groovy
+++ grails-app/controllers/foodprint/ReportViewerController.groovy
@@ -13,9 +13,24 @@ class ReportViewerController {
     	def rest = new RestBuilder()
 
 		rest.restTemplate.setMessageConverters([new StringHttpMessageConverter(Charset.forName("UTF-8"))])
-		def resp = rest.get("http://192.168.2.104:8100/SFT/ws/demo/records/GinPin/410002/981009-410002")
+		def url = "http://192.168.2.104:8100/SFT/"
+		def imgUrl = url+"PDA/"
+		def resp = rest.get(url+"ws/demo/records/GinPin/410002/981009-410002")
 
   		def records = new XmlParser().parseText(resp.text)
+
+		def formImg=records.form.field.findAll{ field->
+				field.'@label'=="圖片"
+  			}
+  		formImg.each{
+  			it.value=imgUrl+it.text()
+  		}
+
+  		records.tabs.tab.detail.row.cell.each{ cell->
+  			if(cell.img.size()>0){
+  				cell.img[0].'@src'	= [imgUrl+ cell.img.'@src'[0]]
+			}
+  		}
 
   		return [reportData: records]
 
