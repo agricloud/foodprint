f9ec4704e018775c36923dc884a1ef00ce54808d
Spooky <smlsun@gmail.com>
2013-09-27 11:58:22 +0800

    modify batchSource domain

--- grails-app/domain/foodprint/BatchSource.groovy
+++ grails-app/domain/foodprint/BatchSource.groovy
@@ -4,12 +4,33 @@ package foodprint
     /*
     * 記錄生產的 batch 是由哪些元物料的 batch 所組成
     */
-class BatchSource extends DefaultTable{
+class BatchSource extends DefaultTable {
 
-	static belongsTo = [parentBatch: Batch]
+
+    /*
+    * 最接近的直接上層批號
+    */
+	static belongsTo = [parent: Batch]
+
+
+    /*
+    * 目前子批號
+    */
 	Batch batch
 
+
+    /*
+    * 最上層成品批號
+    */
+	Batch topParent
+
+    /*
+    * 是否是最底層批號
+    */
+    Boolean isEndBatch = false
+
     static constraints = {
-    	batch unique: 'parentBatch' 
+    	batch unique: 'parent' 
+    	topParent nullable: true
     }
 }
