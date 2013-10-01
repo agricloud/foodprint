d5425b23411c4af949bd988a654c88e1f6786fda
Spooky <smlsun@gmail.com>
2013-10-01 20:17:12 +0800

    remove /rest/* urlMapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,17 +7,6 @@ class UrlMappings {
             }
         }
 
-        "/rest/$controller/$action?/$id?" {
-            constraints {
-                // apply constraints here
-            }
-        }		
-		// "/rest/$controller/$action?/$id?(.${format})?" {
-		// 	constraints {
-		// 		// apply constraints here
-		// 	}
-		// }
-
 
 		"/"(controller: "home", action: "index")
 		"500"(view:'/error')
