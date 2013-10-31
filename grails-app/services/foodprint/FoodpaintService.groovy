22fe1c2f0a5a6982d049dd561d7be97f23e59652
Pipi <pipi@Pipis-MacBook-Air.local>
2013-10-31 18:19:23 +0800

    調整dataImport foodpaint to foodprint

--- grails-app/services/foodprint/FoodpaintService.groovy
+++ grails-app/services/foodprint/FoodpaintService.groovy
@@ -40,14 +40,15 @@ class FoodpaintService {
      */
     def doDataImport() {
 
-        if (!ping()) {
-            return
-        }
+        // if (!ping()) {
+        //     return
+        // }
 
         def rest = new RestBuilder()
         rest.restTemplate.setMessageConverters([new StringHttpMessageConverter(Charset.forName("UTF-8"))])
 
         def url = "${grailsApplication.config.grails.foodpaint.service.api.url}/queryBatchReport"
+        url = "http://localhost:8180/api/queryBatchReport"
         def resp = rest.get(url)
 
 
@@ -58,17 +59,157 @@ class FoodpaintService {
     }
 
     private importData(jsonString){
-        log.debug "ReportViewerController--importData"
+        log.debug "FoodpaintService--importData"
 
         def records=JSON.parse(jsonString)
 
-        println "records.item "+records.item.size()
+        def importClassList = [
+                'item',
+                'workstation',
+                'operation',
+                'supplier',
+                'customer',
+                'batch',
+                'batchSource'
+            ]
+
+        
+        importClassList.each{
+
+            println "records.${it}資料筆數 == "+records[it].size()
+
+            records[it].each{ object ->
+
+                println object as JSON
+
+                object = processDefaultTable(object)
+
+                def domain
+
+                if(it == "item")
+                    domain=getItemInstance(object)
+                if(it == "workstation")
+                    domain=getWorkstationInstance(object)
+                if(it == "operation")
+                    domain=getOperationInstance(object)
+                if(it == "supplier")
+                    domain=getSupplierInstance(object)
+                if(it == "customer")
+                    domain=getCustomerInstance(object)
+                if(it == "batch"){
+                    object.item = Item.findByName(object.item.name)
+                    object.supplier = Supplier.findByName(object.supplier.name)
+                    domain=getBatchInstance(object)
+                }
+                if(it == "batchSource"){
+                    object.batch = Batch.findByName(object.batch.name)
+                    object.childBatch = Batch.findByName(object.childBatch.name)
+                    domain=getBatchSourceInstance(object)
+                }
+
+                domain.properties = object
+                domain.save(flush: true, failOnError:true) 
+            }
+
+        }//end importClassList
+
+        log.debug "品項清單："
+        Item.list().each{
+            log.debug it.name+"/"+it.title
+        }
+
+    }
+    def private getItemInstance(object){
+
+        def domain = Item.findByName(object.name)
+        if(!domain){
+            domain = new Item(name:object.name)
+        }
+        domain
+    }
+
+    def private getWorkstationInstance(object){
+
+        def domain = Workstation.findByName(object.name)
+        if(!domain){
+            domain = new Workstation(name:object.name)
+        }    
+        domain
+    }
+
+    def private getOperationInstance(object){
+
+        def domain = Operation.findByName(object.name)
+        if(!domain){
+            domain = new Operation(name:object.name)
+        }    
+        domain
+    }
+
+    def private getSupplierInstance(object){
+
+        def domain = Supplier.findByName(object.name)
+        if(!domain){
+            domain = new Supplier(name:object.name)
+        }   
+        domain
+    }
+
+    def private getCustomerInstance(object){
+
+        def domain = Customer.findByName(object.name)
+        if(!domain){
+            domain = new Customer(name:object.name)
+        }    
+        domain
+    }
+
+    def private getBatchInstance(object){
+
+        def domain = Batch.findByName(object.name)
+        if(!domain){
+            domain = new Batch(name:object.name)
+        }    
+        domain
+    }
+
+    def private getBatchSourceInstance(object){
+
+        def domain = BatchSource.findByBatchAndChildBatch(object.batch,object.childBatch)
+        if(!domain){
+            domain = new BatchSource(batch:object.batch,childBatch:object.childBatch)
+        }
+        domain
+    }
+
+    def private processDefaultTable(object){
+        object.site = Site.findByName(object.site.name)
+
+        object.lastUpdated = Date.parse("yyyy-MM-dd'T'HH:mm:ss'Z'",object.lastUpdated)//Date.parse('yyyyMMdd',record.incomingDate.text())
+        object.dateCreated = Date.parse("yyyy-MM-dd'T'HH:mm:ss'Z'",object.dateCreated)
+
+        object
+    }
+
+    /*
+
+        log.debug "records.item =="+records.item.size()
+        log.debug "records.workstation =="+records.workstation.size()
+
         //匯入品項
-        records.item.each{ item ->
-            item.site = Site.findByName(item.site.name)
-            item.lastUpdated = null
-            item.dateCreated = null
-            new Item(item).save(flush: true, failOnError:true)
+        records.item.each{ object ->
+            object.site = Site.findByName(object.site.name)
+
+            object.lastUpdated = Date.parse("yyyy-MM-dd'T'HH:mm:ss'Z'",object.lastUpdated)//Date.parse('yyyyMMdd',record.incomingDate.text())
+            object.dateCreated = Date.parse("yyyy-MM-dd'T'HH:mm:ss'Z'",object.dateCreated)
+
+            def domain = Item.findByName(object.name)
+            if(!domain){
+                domain = new Item(name:object.name)
+            }    
+            
+            domain.properties = object
+            domain.save(flush: true, failOnError:true)
         }
 
         log.debug "品項清單："
@@ -76,21 +217,77 @@ class FoodpaintService {
             log.debug it.name+"/"+it.title
         }
 
+        records.workstation.each{ object ->
+            object.site = Site.findByName(object.site.name)
+            def domain = Workstation.findByName(object.name)
+            if(!domain){
+                domain = new Workstation(name:object.name)
+            }    
+            
+            domain.properties = object
+            domain.save(flush: true, failOnError:true)
+        }
+
+        records.operation.each{ object ->
+            object.site = Site.findByName(object.site.name)
+            def domain = Operation.findByName(object.name)
+            if(!domain){
+                domain = new Operation(name:object.name)
+            }    
+            
+            domain.properties = object
+            domain.save(flush: true, failOnError:true)
+        }
+
+        records.supplier.each{ object ->
+            object.site = Site.findByName(object.site.name)
+            def domain = Supplier.findByName(object.name)
+            if(!domain){
+                domain = new Supplier(name:object.name)
+            }    
+            
+            domain.properties = object
+            domain.save(flush: true, failOnError:true)
+        }
+
+        records.customer.each{ object ->
+            object.site = Site.findByName(object.site.name)
+            def domain = Customer.findByName(object.name)
+            if(!domain){
+                domain = new Customer(name:object.name)
+            }    
+            
+            domain.properties = object
+            domain.save(flush: true, failOnError:true)
+        }
+
         //匯入批號
-        records.batch.each{ batch ->
-            batch.site=Site.findByName(batch.site.name)
-            batch.item=Item.findByName(batch.item.name)
-            batch.lastUpdated = null
-            batch.dateCreated = null
-            new Batch(batch).save(flush: true, failOnError:true)
+        records.batch.each{ object ->
+            object.site = Site.findByName(object.site.name)
+            object.item = Item.findByName(object.item.name)
+            
+            def domain = Batch.findByName(object.name)
+            if(!domain){
+                domain = new Batch(name:object.name)
+            }    
+            
+            domain.properties = object
+            domain.save(flush: true, failOnError:true)
         }
         //匯入批號關聯
-        records.batchSources.each{ batchSource ->
-            batchSource.batch=Batch.findByName(batchSource.batch.name)
-            batchSource.childBatch=Batch.findByName(batchSource.childBatch.name)
-            batchSource.lastUpdated = null
-            batchSource.dateCreated = null
-            new BatchSource(batchSource).save(flush: true, failOnError:true)
+        records.batchSource.each{ object ->
+            object.batch = Batch.findByName(object.batch.name)
+            object.childBatch = Batch.findByName(object.childBatch.name)
+
+            def domain = BatchSource.findByBatchAndChildBatch(object.batch,object.childBatch)
+
+            if(!domain){
+                domain = new BatchSource(batch:object.batch,childBatch:object.childBatch)
+            }    
+            
+            domain.properties = object
+            domain.save(flush: true, failOnError:true)
         }
-    }
+
+    */
 }
--- test/unit/foodprint/FoodpaintServiceTests.groovy
+++ test/unit/foodprint/FoodpaintServiceTests.groovy
@@ -4,14 +4,26 @@ package foodprint
 
 import grails.test.mixin.*
 import org.junit.*
+import grails.converters.*
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(FoodpaintService)
+@Mock([
+	Site,
+    Item,
+    Customer,
+    Workstation,
+    Operation,
+    Supplier,
+    Batch,
+    BatchSource])
 class FoodpaintServiceTests {
 
     void testSomething() {
-        fail "Implement me"
+    	service.doDataImport()
+
+        //fail "Implement me"
     }
 }
