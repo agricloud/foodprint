099703f7d459dcce8152e7c25bcc13e77149bdc4
Pipi <ppelize@gmail.com>
2013-11-15 12:52:27 +0800

    batchRoute ConvertServiceTest

--- grails-app/services/common/ConvertService.groovy
+++ grails-app/services/common/ConvertService.groovy
@@ -1,6 +1,6 @@
 package common
 import org.codehaus.groovy.grails.commons.DefaultGrailsDomainClass
-
+import grails.converters.JSON
 class ConvertService {
 
     def getDomainFields(domainClassName) {
@@ -28,6 +28,7 @@ class ConvertService {
     def batchParseJson(batch){
 	    def result = [:]
 	    result.id= batch.id
+	    result.name=batch.name
 	    result.item = batch.item
         result["item.id"] = batch.item.id
         result["item.name"] = batch.item.name
@@ -41,8 +42,70 @@ class ConvertService {
     def itemParseJson(item){
 	    def result = [:]
 	    result.id = item.id
-	    result.name = "test"
+	    result.name = item.name
+	    result.title = item.title
 
 	    result
     }
+
+    def operationParseJson(operation){
+	    def result = [:]
+	    result.id= operation.id
+	    result.name = operation.name
+	    result.title = operation.title
+	    result.description = operation.description
+	    
+	    result
+    }
+
+    def workstationParseJson(workstation){
+	    def result = [:]
+	    result.id= workstation.id
+	    result.name = workstation.name
+	    result.title = workstation.title
+	    result.description = workstation.description
+	    
+	    result
+    }
+
+    def supplierParseJson(supplier){
+	    def result = [:]
+	    result.id= supplier.id
+	    result.name = supplier.name
+	    result.title = supplier.title
+	    result.country = supplier.country
+	    result.tel = supplier.tel
+	    result.email = supplier.email
+	    result.address = supplier.address
+	    
+	    result
+    }
+
+   	def batchRouteParseJson(batchRoute){
+   		def result = [:]
+   		result.id=batchRoute.id
+   		result.sequence=batchRoute.sequence
+   		result.batch=batchRoute.batch
+   		result["batch.id"] = batchRoute.batch.id
+   		result["batch.name"] = batchRoute.batch.name
+   		result.operation=batchRoute.operation
+        result["operation.id"] = batchRoute.operation.id
+        result["operation.name"] = batchRoute.operation.name
+        result["operation.title"] = batchRoute.operation.title
+
+        if(batchRoute.workstation){
+        	result.workstation=batchRoute.workstation
+            result["workstation.id"] = batchRoute.workstation.id
+            result["workstation.name"] = batchRoute.workstation.name
+            result["workstation.title"] = batchRoute.workstation.title
+        }
+        if(batchRoute.supplier){
+        	result.supplier=batchRoute.supplier
+            result["supplier.id"] = batchRoute.supplier.id
+            result["supplier.name"] = batchRoute.supplier.name
+            result["supplier.title"] = batchRoute.supplier.title
+        }
+
+        result
+   	}
 }
