cf41e91f309c3322a13dd6f8e89d71d3fcbc6d52
Spooky <smlsun@gmail.com>
2013-11-27 12:19:26 +0800

    update batchSpec

--- test/unit/foodprint/BatchSpec.groovy
+++ test/unit/foodprint/BatchSpec.groovy
@@ -8,18 +8,20 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
 @TestMixin(GrailsUnitTestMixin)
+@TestFor(Batch)
+@Mock([Item])
 class BatchSpec extends Specification {
 
-    def setup() {
-    }
+    void "測試新增批號與取得清單"() {
+    	given: '產生一個測試批號 domain'
+    	def item = new Item(name:'item').save()
+    	new Batch(name: 'batch', item:item).save()
 
-    def cleanup() {
-    }
+    	when: '要求取得批號清單'
+    	def batchList = Batch.list()
 
-    void "簡單測試一下"() {
-   		expect:
-      		1 == 1
-      		1 == 2
+   		then: '清單中要有一個批號資訊'
+      		batchList.size() == 1
 
     }
 }
