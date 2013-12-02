ccb36e6be7c5309fd3031afc3210b363ecb96180
Spooky <smlsun@gmail.com>
2013-12-02 18:30:07 +0800

    Ignore S3AttachmentServiceTests,  需要時請手動測試

--- test/unit/foodprint/S3AttachmentServiceTests.groovy
+++ test/unit/foodprint/S3AttachmentServiceTests.groovy
@@ -9,61 +9,67 @@ import common.*
 @Mock([S3Service, ImageModiService])
 class S3AttachmentServiceTests {
 	
-    
-  //   void testS3Save() {
-  //   	service.fileLocation = "test"
+    @Ignore
+    void testS3Save() {
+    	service.fileLocation = "test"
 
-		// def params =[:]
-		// params.file = [:]
-		// params.file.inputStream = new FileInputStream("test/test.jpg");
-		// params.domainName = "unitTest"
-		// params.domainId = "1"
+		def params =[:]
+		params.file = [:]
+		params.file.inputStream = new FileInputStream("test/test.jpg");
+		params.domainName = "unitTest"
+		params.domainId = "1"
         
-  //       assert service.save(params).success
-  //   }
-  //   void testS3ShowAndScaleToFile() {
-  //   	service.fileLocation = "test"
+        assert service.save(params).success
+    }
 
-		// def params =[:]
-		// params.domainName = "unitTest"
-		// params.id = "1"
+    @Ignore
+    void testS3ShowAndScaleToFile() {
+    	service.fileLocation = "test"
+
+		def params =[:]
+		params.domainName = "unitTest"
+		params.id = "1"
         
-  //       def s3Image = service.show(params) //dataInputStream
-  //       assert s3Image	
+        def s3Image = service.show(params) //dataInputStream
+        assert s3Image	
 
 
-  //       def imageModiService = new ImageModiService();
-  //       InputStream inputStream = s3Image;
-  //       def byteArrayOutputStream=imageModiService.sizeNormal(inputStream)
-  //       OutputStream outputStream = new FileOutputStream ("S3-normal.jpg"); 
-  //       byteArrayOutputStream.writeTo(outputStream);
+        def imageModiService = new ImageModiService();
+        InputStream inputStream = s3Image;
+        def byteArrayOutputStream=imageModiService.sizeNormal(inputStream)
+        OutputStream outputStream = new FileOutputStream ("S3-normal.jpg"); 
+        byteArrayOutputStream.writeTo(outputStream);
 
-  //   }
-  //   void testS3ShowAndScaleToS3() {
-  //   	service.fileLocation = "test"
+    }
 
-		// def params =[:]
-		// params.domainName = "unitTest"
-		// params.id = "1"
+    @Ignore
+    void testS3ShowAndScaleToS3() {
+    	service.fileLocation = "test"
+
+		def params =[:]
+		params.domainName = "unitTest"
+		params.id = "1"
         
-  //       def s3Image = service.show(params) //dataInputStream
-  //       assert s3Image	
+        def s3Image = service.show(params) //dataInputStream
+        assert s3Image	
 
 
-  //       def imageModiService = new ImageModiService();
-  //       InputStream inputStream = s3Image;
-  //       def byteArrayOutputStream=imageModiService.sizeNormal(inputStream)
+        def imageModiService = new ImageModiService();
+        InputStream inputStream = s3Image;
+        def byteArrayOutputStream=imageModiService.sizeNormal(inputStream)
 
 
 
-		// ByteArrayInputStream inputStreamScaled = new ByteArrayInputStream(byteArrayOutputStream.toByteArray())
-		// params.file = [:]
-		// params.file.inputStream = inputStreamScaled
-		// params.domainName = "unitTest"
-		// params.domainId = "1-normal"
+		ByteArrayInputStream inputStreamScaled = new ByteArrayInputStream(byteArrayOutputStream.toByteArray())
+		params.file = [:]
+		params.file.inputStream = inputStreamScaled
+		params.domainName = "unitTest"
+		params.domainId = "1-normal"
         
-  //       assert service.save(params).success
+        assert service.save(params).success
 
-  //   }
+    }
+    void testSomething() {
 
+    }
 }
