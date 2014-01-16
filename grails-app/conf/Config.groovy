9473d8bb4dd6d2faa02a898d29dec5820cebbc63
Pipi <ppelize@gmail.com>
2014-01-16 23:11:07 +0800

    關閉database migration自動更新

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -122,8 +122,8 @@ environments {
                 'grails.app.jobs'
             ]
         }
-        grails.plugin.databasemigration.updateOnStart = true
-        grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
+        // grails.plugin.databasemigration.updateOnStart = true
+        // grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
     }
  
     test {
