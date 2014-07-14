ba24ae91440e71b9e2520d470a1c67a2f9ebca50
Pipi <ppelize@gmail.com>
2014-07-15 05:31:05 +0800

    fix for server update

--- grails-app/migrations/1.0.4.groovy
+++ grails-app/migrations/1.0.4.groovy
@@ -1,12 +1,3 @@
 databaseChangeLog = {
 
-	changeSet(author: "pipi (generated)", id: "1405370838862-1") {
-		dropIndex(indexName: "unique_name", tableName: "batch")
-	}
-
-	changeSet(author: "pipi (generated)", id: "1405370838862-2") {
-		createIndex(indexName: "name_uniq_1405370838367", tableName: "batch", unique: "true") {
-			column(name: "name")
-		}
-	}
 }
