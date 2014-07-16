5e263a793f2fcf1cf9a3da9ec85274dfe4ff0c26
Pipi <ppelize@gmail.com>
2014-07-16 16:24:14 +0800

    訂單單身維護 加入顯示品項單位欄位

--- extjs-app/app/model/ErpCustomerOrderDet.js
+++ extjs-app/app/model/ErpCustomerOrderDet.js
@@ -42,6 +42,9 @@ Ext.define('foodprint.model.ErpCustomerOrderDet', {
             name: 'item.title'
         },
         {
+            name: 'item.unit'
+        },
+        {
             name: 'qty'
         }
     ],
