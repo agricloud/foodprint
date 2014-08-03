9c489c0b9668af37058cc4729b0b7783ab794fb6
r85306474 <r85306474gmail.com>
2014-08-03 18:51:01 +0800

    修正問題

--- extjs-app/app/controller/ErpAccountSheetController.js
+++ extjs-app/app/controller/ErpAccountSheetController.js
@@ -39,7 +39,7 @@ Ext.define('foodprint.controller.ErpAccountSheetController', {
         'ErpSaleReturnSheetDetStore'
     ],
     views: [
-        null
+        'ErpAccountSheetView'
     ],
 
     refs: [
